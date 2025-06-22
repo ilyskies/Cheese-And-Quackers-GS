73d08b98f59fcb2cdcbb3910a7773228574023d7
swayzz <167585207+medicalsectors@users.noreply.github.com>
2025-06-22 21:07:05 +0100

    ADDED BATCH FILE FOR STARTING HXNTEDS PYTHON FILE

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Start the Go server and keep the console open after exit
+
+echo Starting Cheese And Quackers GS...
+start Start.py
+
+echo.
+echo GS STOPPED, KILL YOURSELF NIGGA.
+pause >nul 
