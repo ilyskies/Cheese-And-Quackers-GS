3f92a409b4a0eb0d81efa5859948d6b2893e3f99
Syphon <144607268+Syphonfr@users.noreply.github.com>
2025-06-22 12:05:53 -0500

    /_ |   |__ \   |__ \  |___  |    | |      ) |     ) |    / /    | |     __/     __/     / /    | |   |__ \   |__ \    / /   |_|    |___/   |___/    /_/)

--- Cheese And Quackers GS/Cheese And Quackers GS.cpp
+++ Cheese And Quackers GS/Cheese And Quackers GS.cpp
@@ -2,9 +2,18 @@
 
 int main()
 {
-    std::cout << "Start GS: INITIATED.\n"; // This starts the game server
+    std::wstring onethreethreeseven = LR"(   __     ___     ___    _____
+  /_ |   |__ \   |__ \  |___  |
+   | |      ) |     ) |    / /
+   | |     __/     __/     / /
+   | |   |__ \   |__ \    / /
+  |_|    |___/   |___/    /_/)";
+
+    std::wcout << onethreethreeseven << std::endl;
+
+    std::cout << "\nStart GS: INITIATED.\n"; // This starts the game server
     std::cout << "UD CODE: Sigma Number: " << 1337 << std::endl;
-    
+
     std::cout << "Detecting Mstreem pasted code..." << std::endl;
     std::cout << "Pasted code detected." << std::endl;
 }
